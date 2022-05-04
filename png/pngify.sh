3d892d0de03dfffe544801b59dbee5d3f009d09e
Alex <53974096+ExalFabu@users.noreply.github.com>
2022-05-04 09:17:26 +0200

    Initial Commit

---
+++ png/pngify.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+for svg in /home/krasto/workspace/OGC/Pharmagc/UML/svg/*svg 
+do
+  png=$(echo $svg | sed "s/svg/png/gi");
+  inkscape -w 1080 --export-type png -b white "$svg" -o "$png"
+done;
