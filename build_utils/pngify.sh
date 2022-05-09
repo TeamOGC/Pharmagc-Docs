311bfc1f8e809367b5a005597ee3c4bb280edd3a
Alex <53974096+ExalFabu@users.noreply.github.com>
2022-05-09 18:05:02 +0200

    Gestione Farmaci + Scaleup pngs

--- build_utils/pngify.sh
+++ build_utils/pngify.sh
@@ -3,5 +3,5 @@ rm -rf /home/krasto/workspace/OGC/Pharmagc/UML/png/*
 for svg in /home/krasto/workspace/OGC/Pharmagc/UML/svg/*svg 
 do
   png=$(echo $svg | sed "s/svg/png/gi");
-  inkscape -w 1080 --export-type png -b white "$svg" -o "$png"
+  inkscape -w 2160 --export-type png -b white "$svg" -o "$png"
 done;
